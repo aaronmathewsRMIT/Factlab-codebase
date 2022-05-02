6a0057973cd7df78c587771aeefa08805610c0c6
aaronmathewsRMIT <s3854257@student.rmit.edu.au>
2022-05-02 16:45:37 +1000

    Update forms.py

--- FactlabUI/claim_app/forms.py
+++ FactlabUI/claim_app/forms.py
@@ -1,8 +1,3 @@
 from attr import field
 from django.forms import ModelForm
 from .models import claim
-
-class claimForm(ModelForm):
-    class Meta:
-        model = claim
-        fields = '__all__'
